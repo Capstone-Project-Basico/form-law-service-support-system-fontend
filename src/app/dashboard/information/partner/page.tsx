217b4cb1a031f81e57ef884477a55e551e3b1f45
hoaiphuc <nguyenhoaiphuc3122001@gmail.com>
2024-03-23 16:17:58 +0700

    update partner page

--- src/app/dashboard/information/partner/page.tsx
+++ src/app/dashboard/information/partner/page.tsx
@@ -42,15 +42,16 @@ import Image from "next/image";
 const Partner = () => {
   const [partners, setPartners] = useState<Partner[]>([]);
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
-  // Fallback image URL
-  const [imageError, setImageError] = useState(false);
-  const fallbackImageUrl = "/fallback-image.png";
-
+  const [searchTerm, setSearchTerm] = useState("");
+  const {
+    isOpen: isOpenUpdate,
+    onOpen: onOpenUpdate,
+    onClose: onCloseUpdate,
+  } = useDisclosure();
   //upload file
   const [imageUpload, setImageUpload] = useState<File | null>(null);
-  const [imageUrls, setImageUrls] = useState<string[]>([]);
-  const [imageUrl, setImageUrl] = useState<string>();
+  // const [imageUrls, setImageUrls] = useState<string[]>([]);
+  // const [imageUrl, setImageUrl] = useState<string>();
 
   const imagesListRef = ref(storage, "partners/");
 
@@ -58,8 +59,8 @@ const Partner = () => {
   const [name, setName] = useState("");
   const [avatar, setAvatar] = useState("");
   const [link, setLink] = useState("");
-  const [imageRef, setImageRef] = useState<StorageReference | undefined>();
-
+  // const [imageRef, setImageRef] = useState<StorageReference | undefined>();
+  const [selectedPartner, setSelectedPartner] = useState<Partner | null>(null);
   let newPartner = {
     name,
     avatar,
@@ -67,64 +68,37 @@ const Partner = () => {
   };
 
   useEffect(() => {
-    const fetchPartners = async () => {
-      try {
-        const response = await axios.get(
-          `${process.env.NEXT_PUBLIC_BASE_API}partner/getAllPartners`
-        );
-        console.log(response.data);
-        setPartners(response.data.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
     fetchPartners();
   }, []);
 
-  useEffect(() => {
-    getURL();
-  }, [imageRef]);
-  const getURL = () => {
-    console.log(imageRef?.fullPath);
-    if (imageRef?.fullPath != null) {
-      getDownloadURL(imageRef).then((url) => console.log(url));
+  //search
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+  // Filter partners based on search term
+  const filteredPartners = partners.filter((partner) =>
+    partner.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  //get all items
+  const fetchPartners = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_BASE_API}partner/getAllPartners`
+      );
+      setPartners(response.data.data);
+      // setPartners((prevPartners) => [...prevPartners, response.data.data]);
+    } catch (error) {
+      console.error(error);
     }
   };
+
+  // };
   //upload file
   const uploadFile = (e: any) => {
     setImageUpload(e.target.files[0]);
     let image = e.target.files[0];
 
     if (image == null) return;
-    // const imgref = ref(storage, `partners/${image.name + uuidv4()}`);
-    // setImageRef(imgref);
-
-    // uploadBytes(imgref, image).then((snapshot) => {
-    //   getDownloadURL(snapshot.ref).then((url) => {
-    //     setImageUrls((prev) => [...prev, url]);
-    //   });
-    // });
-
-    // listAll(imagesListRef).then((response) => {
-    //   response.items.forEach((item) => {
-    //     // if(item.path ==)
-    //   });
-    // });
-
-    // const firebase = admin.initializeApp();
-    // const storage = firebase.storage;
-
-    //2
-    // storage
-    //   .ref("/images/" + image.name)
-    //   .put(image)
-    //   .on("State_changed", alert("success"), alert, () => {
-    //     storage
-    //       .ref("images")
-    //       .child(image.name)
-    //       .getDownloadURL()
-    //       .then((url: any) => console.log(url));
-    //   });
     const storageRef = ref(storage, "/images/" + image.name + uuidv4()); // Create a reference to '/images/imageName'
 
     const uploadTask = uploadBytesResumable(storageRef, image); // Start the file upload
@@ -152,6 +126,58 @@ const Partner = () => {
     );
   };
 
+  //upload update file
+  const uploadUpdateFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // First, check if the files array is not null and has at least one file
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0]; // Safely access the first file
+
+      // Create a unique file name for the storage to avoid name conflicts
+      const uniqueFileName = `partners/${file.name}-${uuidv4()}`;
+      const storageRef = ref(storage, uniqueFileName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file); // Start the file upload
+
+      // Listen for state changes, errors, and completion of the upload.
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // Optional: monitor upload progress
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          // Handle unsuccessful uploads
+          console.error("Upload failed", error);
+        },
+        () => {
+          // Handle successful uploads on complete
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+
+            if (!selectedPartner) {
+              console.error("No partner selected for update.");
+              return;
+            }
+
+            // Update the state with the new avatar URL
+            setSelectedPartner({
+              ...selectedPartner,
+              avatar: downloadURL,
+            });
+
+            // Optionally: Update the partner's information in the database or state here
+            // This might involve calling an API endpoint or updating local state
+          });
+        }
+      );
+    } else {
+      console.error("No file selected for upload.");
+    }
+  };
+
+  //add a new partner
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     axios
@@ -159,10 +185,68 @@ const Partner = () => {
         `${process.env.NEXT_PUBLIC_BASE_API}partner/createNewPartner`,
         newPartner
       )
+
       .then((response) => {
-        console.log("thanh cong");
+        setPartners((prevPartners) => [...prevPartners, response.data.data]);
       })
-      .catch((error) => {});
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  //update
+  const handleUpdateSubmit = async () => {
+    if (!selectedPartner) return; // Check if a partner is selected
+
+    // Example: PUT request to update partner details
+    axios
+      .put(
+        `${process.env.NEXT_PUBLIC_BASE_API}partner/updatePartner/${selectedPartner.partnerId}`,
+        {
+          name: selectedPartner.name,
+          avatar: selectedPartner.avatar,
+          link: selectedPartner.link,
+        }
+      )
+      .then((response) => {
+        fetchPartners();
+        console.log("Partner updated successfully", response);
+      })
+      .catch((error) => {
+        console.error("Failed to update partner", error);
+      });
+  };
+
+  //delete
+  const handleDelete = async (partnerId: number) => {
+    const isConfirmed = window.confirm(
+      "Bạn có chắc muốn xóa đối tác này không?"
+    );
+    if (isConfirmed) {
+      try {
+        const userString = localStorage.getItem("user"); // Assuming the token is stored with the key "token"
+        if (!userString) {
+          console.log("No user found");
+          return;
+        }
+        const user = JSON.parse(userString);
+
+        axios.delete(
+          `${process.env.NEXT_PUBLIC_BASE_API}partner/deletePartner/${partnerId}`
+        ),
+          {
+            headers: {
+              Authorization: user.data.data.token,
+            },
+          };
+
+        setPartners((prevPartners) =>
+          prevPartners.filter((partner) => partner.partnerId !== partnerId)
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
   return (
     <div className="w-full mt-5 ml-5 mr-5">
@@ -211,10 +295,10 @@ const Partner = () => {
                     </ModalBody>
                     <ModalFooter>
                       <Button color="danger" variant="light" onPress={onClose}>
-                        Close
+                        Đóng
                       </Button>
                       <Button color="primary" onPress={onClose} type="submit">
-                        Action
+                        Thêm
                       </Button>
                     </ModalFooter>
                   </form>
@@ -228,16 +312,18 @@ const Partner = () => {
         <div className="my-10 flex flex-row">
           <Input
             classNames={{
-              base: "max-w-full sm:max-w-[10rem] h-10",
+              base: "w-full sm:max-w-[10rem] h-10",
               mainWrapper: "h-full",
               input: "text-small",
               inputWrapper:
                 "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
             }}
-            placeholder="Từ khóa tìm kiếm"
+            placeholder="Từ khóa tìm kiếm .."
             size="sm"
             type="search"
             radius="none"
+            value={searchTerm}
+            onChange={handleSearchChange}
           />
           <Button className="bg-[#FF0004] text-white ml-3" radius="none">
             Tìm kiếm
@@ -263,16 +349,18 @@ const Partner = () => {
           </TableColumn>
         </TableHeader>
         <TableBody>
-          {partners.map((partner, index) => (
+          {filteredPartners.map((partner, index) => (
             <TableRow key={index}>
               <TableCell>{partner.name}</TableCell>
               <TableCell>
                 {
                   <Image
                     src={
-                      partner.avatar.startsWith("http")
-                        ? partner.avatar
-                        : `/${partner.avatar}`
+                      partner.avatar
+                        ? partner.avatar.startsWith("http")
+                          ? partner.avatar
+                          : `/${partner.avatar}`
+                        : "/errorImage.png"
                     }
                     alt=""
                     width={100}
@@ -287,13 +375,80 @@ const Partner = () => {
                 {partner.delete ? "Không sử dụng" : "Đang hoạt động"}
               </TableCell>
               <TableCell className="flex gap-2 items-center">
-                <Button>Update</Button>
-                <Button>Delete</Button>
+                <Button
+                  className="bg-[#FF0004] text-white"
+                  onPress={() => {
+                    setSelectedPartner(partner);
+                    onOpenUpdate();
+                  }}
+                >
+                  Update
+                </Button>
+
+                <Button
+                  className="bg-[#FF0004] text-white"
+                  onClick={() => handleDelete(partner.partnerId)}
+                >
+                  Delete
+                </Button>
               </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
+
+      {/* update modal */}
+      <Modal isOpen={isOpenUpdate} onClose={onCloseUpdate}>
+        <ModalContent>
+          <ModalHeader className="flex flex-col gap-1">
+            Cập nhật đối tác
+          </ModalHeader>
+          <ModalBody>
+            {selectedPartner && (
+              <form onSubmit={handleUpdateSubmit}>
+                <Input
+                  type="text"
+                  label="Name"
+                  value={selectedPartner.name}
+                  onChange={(e) =>
+                    setSelectedPartner({
+                      ...selectedPartner,
+                      name: e.target.value,
+                    })
+                  }
+                />
+                <input type="file" onChange={(e) => uploadUpdateFile(e)} />
+                <Input
+                  type="text"
+                  label="Link"
+                  value={selectedPartner.link}
+                  onChange={(e) =>
+                    setSelectedPartner({
+                      ...selectedPartner,
+                      link: e.target.value,
+                    })
+                  }
+                />
+              </form>
+            )}
+          </ModalBody>
+          <ModalFooter>
+            <Button color="danger" variant="light" onPress={onCloseUpdate}>
+              Đóng
+            </Button>
+            <Button
+              color="primary"
+              onPress={() => {
+                handleUpdateSubmit();
+                onCloseUpdate();
+              }}
+              type="submit"
+            >
+              Cập nhật
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 };
