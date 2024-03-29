d9a963bd32e7da5581a09932e0f19adb6b16525f
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2024-03-29 23:44:58 +0700

    template version

--- src/app/dashboard/template/add-template/page.tsx
+++ src/app/dashboard/template/add-template/page.tsx
@@ -18,7 +18,7 @@ export default function AddTemplatePage(props: IAddTemplatePageProps) {
   const [file, setFile] = React.useState<File | null>(null);
   const [titleInputError, setTitleInputError] = React.useState<string>('');
   const [formTypes, setFormTypes] = React.useState<FormType[]>([]);
-  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
 
   React.useEffect(() => {
     async function fetchformTypes() {
@@ -147,7 +147,7 @@ export default function AddTemplatePage(props: IAddTemplatePageProps) {
         <div className="flex justify-center items-center space-x-1 text-sm text-gray-700">
           <svg fill="none" className="w-6 h-6 animate-spin" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
             <path
-              clip-rule="evenodd"
+              clipRule="evenodd"
               d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
               fill="currentColor"
               fill-rule="evenodd"
--- src/lib/axiosClient.tsx
+++ src/lib/axiosClient.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 const getToken = () => {
   if (typeof window === 'undefined') return null;
 
-  return JSON.parse(localStorage.getItem('auth-token') || '');
+  return JSON.parse(localStorage.getItem('auth-token') || '{}');
 };
 
 const axiosClient = axios.create({
