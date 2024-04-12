3d7f5fd66fac2b0ca7373073a788b355d5f8ad22
hoaiphuc <nguyenhoaiphuc3122001@gmail.com>
2024-04-12 15:52:33 +0700

    update not found page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,12 +1,20 @@
-import Link from "next/link";
 import React from "react";
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col justify-center items-center my-[215px]">
+      <FontAwesomeIcon
+        icon={faTriangleExclamation}
+        className="size-40 text-[#FF0004]"
+      />
+      <h1 className="text-8xl text-[#FF0004] font-semibold">
+        404 KHÔNG TÌM THẤY
+      </h1>
+      <p className="text-[18px] font-semibold mt-10">
+        File này có thể đã bị di chuyển hoặc xóa, bạn kiểm tra lại đã viết đúng
+      </p>
     </div>
   );
 };
