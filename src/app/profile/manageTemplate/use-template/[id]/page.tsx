9a4f52ccfcc2a8df4c6ac5435f46799b8699d747
Huynh Tran The Trong <78414286+trongtk001@users.noreply.github.com>
2024-04-12 15:12:06 +0700

    Trong (#93)
    * use template
    
    * fix
    
    * fix
    
    * use template

--- src/app/profile/manageTemplate/use-template/[id]/page.tsx
+++ src/app/profile/manageTemplate/use-template/[id]/page.tsx
@@ -76,16 +76,40 @@ const Page = (props: Props) => {
     <div className="p-5">
       <h1 className="text-2xl font-semibold text-left">Use Template</h1>
       <form onSubmit={handleSubmit} className="">
-        <div className="basis-3/12 mt-5">
-          <Input className="m-2 w-40" variant="bordered" label="Tên biểu mẫu" type="text" name="formName" />
+        <div className="basis-3/12 mt-5 border">
+          <div className="p-4">
+            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+              <div className="relative flex items-center justify-between h-16">
+                <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="flex-shrink-0 flex items-center">
+                    <input
+                      className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                      placeholder="Tên biểu mẫu"
+                      type="text"
+                      name="formName"
+                    />
+                  </div>
+                </div>
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  <button disabled className="bg-primary opacity-20 text-white px-4 py-2 rounded-md text-sm font-medium" aria-label="Preview">
+                    Xem trước
+                  </button>
+                  <button disabled className="bg-primary opacity-20 text-white px-4 py-2 ml-3 rounded-md text-sm font-medium" aria-label="Save">
+                    Lưu lại
+                  </button>
+                  <button type="submit" className="bg-primary text-white px-4 py-2 ml-3 rounded-md text-sm font-medium" aria-label="Download">
+                    Lưu và tải xuống
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* <Input className="m-2 w-40" variant="bordered" label="Tên biểu mẫu" type="text" name="formName" /> */}
         </div>
         <div className="w-[850px] mx-auto">
           <div className="overflow-y-scroll max-h-[39rem] min-h-[39rem]  mx-auto p-10 border-2">
             <div className="content-center min-h-[35rem] p-10 border-1 border-black" ref={templateRef}></div>
           </div>
-          <button type="submit" className="w-full bg-primary hover:bg-red-400 text-white font-bold py-2 px-4 rounded m-5">
-            Submit
-          </button>
         </div>
       </form>
     </div>
