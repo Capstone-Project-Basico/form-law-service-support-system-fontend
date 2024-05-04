a8670ac593212939dd3a7979242477942c53a5f7
hoaiphuc <nguyenhoaiphuc3122001@gmail.com>
2024-05-04 21:46:59 +0700

    add recharge function

--- src/app/template/page.tsx
+++ src/app/template/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { Template } from "@/constants/types/homeType";
+import { Template, FormType } from "@/constants/types/homeType";
 import HeaderComponent from "@/components/header";
 // import CardTemplate from "@/sections/CardTemplate";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { GoogleMaps } from "@/components/ui/GoogleMaps";
@@ -17,6 +17,8 @@ import {
   Input,
   CardFooter,
   Pagination,
+  Autocomplete,
+  AutocompleteItem,
 } from "@nextui-org/react";
 import Link from "next/link";
 import axios from "axios";
@@ -38,7 +40,8 @@ interface UserLocal {
 const Page = () => {
   const router = useRouter();
   const [templates, setTemplates] = useState<Template[]>([]);
-  const [wallet, setWallet] = useState<boolean | null>(null);
+  const [filterValue, setFilterValue] = useState("");
+  const [type, setType] = useState<FormType[]>([]);
 
   const getUserFromStorage = () => {
     if (typeof window !== "undefined") {
@@ -63,28 +66,23 @@ const Page = () => {
       .get(`${process.env.NEXT_PUBLIC_BASE_API}formTemplateVersion`)
       .then((response) => {
         setTemplates(response.data.data);
-        console.log(response.data.data);
       });
   };
 
-  const getWallet = () => {
-    setWallet(null);
+  const getType = async () => {
     axios
-      .get(
-        `${process.env.NEXT_PUBLIC_BASE_API}wallet/getWalletByUser/${userId}`
-      )
+      .get(`${process.env.NEXT_PUBLIC_BASE_API}formTemplateVersion`)
       .then((response) => {
-        setWallet(true);
+        setType(response.data.data);
       })
       .catch((error) => {
-        console.error("Error fetching wallet:", error);
-        setWallet(false);
+        console.log(error);
       });
   };
 
   useEffect(() => {
     getTemplate();
-    getWallet();
+    getType();
   }, []);
 
   const handleBuy = async (formId: number, price: number) => {
@@ -93,7 +91,6 @@ const Page = () => {
         Swal.fire({
           title: "Bạn chưa đăng nhập, bạn có muốn đăng nhập?",
           showDenyButton: true,
-          // showCancelButton: true,
           confirmButtonText: "Có",
           denyButtonText: `Không`,
         }).then((result) => {
@@ -114,41 +111,37 @@ const Page = () => {
           cartRequestList: [{ itemId: formId, quantity, price }],
         };
         {
-          wallet
-            ? axios
-                .post(
-                  `${process.env.NEXT_PUBLIC_BASE_API}order/createOrderFormTemplateDetail`,
-                  updatedDataOrder
-                )
-                .then((response) => {
-                  const orderId = response.data.data;
-                  Swal.fire({
-                    title: "Bạn có chấp nhận thanh toán",
-                    showDenyButton: true,
-                    confirmButtonText: "Có",
-                    denyButtonText: `Không`,
-                  }).then((result) => {
-                    if (result.isConfirmed) {
-                      try {
-                        payForTemplate(orderId);
-                      } catch (error) {
-                        Swal.fire(`${error}`, "", "error");
-                      }
-                    } else if (result.isDenied) {
-                      Swal.fire("Vui lòng thanh toán để sử dụng", "", "info");
-                      return;
-                    }
-                  });
-                })
-            : axios
-                .post(
-                  `${process.env.NEXT_PUBLIC_BASE_API}order/createOrderFormTemplateDetail`,
-                  updatedDataOrder
-                )
-                .then((response) => {
-                  const orderId = response.data.data;
-                  payForTemplateByCash(orderId);
-                });
+          axios
+            .post(
+              `${process.env.NEXT_PUBLIC_BASE_API}order/createOrderFormTemplateDetail`,
+              updatedDataOrder
+            )
+            .then((response) => {
+              const orderId = response.data.data;
+              Swal.fire({
+                title: "Vui lòng chọn phương thức thanh toán",
+                showDenyButton: true,
+                showCancelButton: true,
+                confirmButtonText: "Ví",
+                denyButtonText: `Trực tiếp bằng QR`,
+                cancelButtonText: "Đóng",
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  try {
+                    payForTemplate(orderId);
+                  } catch (error) {
+                    Swal.fire(`${error}`, "", "error");
+                  }
+                } else if (result.isDenied) {
+                  try {
+                    payForTemplateByCash(orderId);
+                  } catch (error) {
+                    Swal.fire(`${error}`, "", "error");
+                  }
+                  return;
+                }
+              });
+            });
         }
       }
     } catch (error) {
@@ -164,7 +157,10 @@ const Page = () => {
         { headers: authHeader() }
       )
       .then((res) => {
-        toast.success(`${res.data.data}`);
+        toast.success("Thanh toán thành công");
+      })
+      .catch((error) => {
+        toast.error("Tài khoản không đủ tiền, vui lòng nạp tại ví");
       });
   };
 
@@ -181,6 +177,48 @@ const Page = () => {
         window.open(res.data.checkoutUrl, "_blank");
       });
   };
+
+  const hasSearchFilter = Boolean(filterValue);
+
+  const filteredItems = useMemo(() => {
+    let filteredUsers = [...templates];
+
+    if (hasSearchFilter) {
+      filteredUsers = filteredUsers.filter((temp) =>
+        temp.message.toLowerCase().includes(filterValue.toLowerCase())
+      );
+    }
+
+    return filteredUsers;
+  }, [templates, hasSearchFilter, filterValue]);
+
+  const onSearchChange = useCallback((value?: string) => {
+    if (value) {
+      setFilterValue(value);
+      setPage(1);
+    } else {
+      setFilterValue("");
+    }
+  }, []);
+
+  const onClear = useCallback(() => {
+    setFilterValue("");
+    setPage(1);
+  }, []);
+
+  //pagination
+  const [page, setPage] = useState(1);
+  const rowsPerPage = 8;
+
+  const pages = Math.ceil(filteredItems.length / rowsPerPage);
+
+  const items = useMemo(() => {
+    const start = (page - 1) * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    return filteredItems.slice(start, end);
+  }, [page, filteredItems]);
+
   return (
     <>
       <HeaderComponent title="BIỂU MẪU" link="BIỂU MẪU" />
@@ -192,30 +230,44 @@ const Page = () => {
             Biểu mẫu
           </h1>
 
-          <div className="ml-auto flex flex-col justify-end items-start w-72">
-            <div className="text-[#FF0004] border-b-1 mb-3 w-72">Tìm kiếm</div>
-            <div className="">
-              <Input
-                classNames={{
-                  base: "w-full sm:max-w-[10rem] h-10 ",
-                  mainWrapper: "h-full w-72",
-                  input: "text-small",
-                  inputWrapper:
-                    "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20 ",
-                }}
-                placeholder="Từ khóa tìm kiếm .."
-                size="sm"
-                type="search"
-                radius="none"
-                // value={searchTerm}
-                // onChange={handleSearchChange}
-              />
+          <div className="ml-auto flex flex-row justify-center items-end">
+            <Autocomplete
+              isRequired
+              defaultItems={type}
+              placeholder="Loại biểu mẫu"
+              className="max-w-xs mr-3"
+              // onSelectionChange={(e) => {
+              //   setStaffId(Number(e));
+              // }}
+            >
+              {(item) => (
+                <AutocompleteItem key={item.id}>
+                  {item.typeName}
+                </AutocompleteItem>
+              )}
+            </Autocomplete>
+
+            <div className="flex flex-col justify-end items-start w-72">
+              <div className="text-[#FF0004] border-b-1 mb-3 w-72">
+                Tìm kiếm
+              </div>
+              <div className="">
+                <Input
+                  isClearable
+                  className="w-[660px] sm:max-w-[44%]"
+                  placeholder="Tìm địa chỉ email"
+                  // startContent={<SearchIcon />}
+                  value={filterValue}
+                  onClear={() => onClear()}
+                  onValueChange={onSearchChange}
+                />
+              </div>
             </div>
           </div>
         </div>
-        {templates && (
+        {items && (
           <div className="grid grid-cols-4 mx-56 gap-10">
-            {templates.map((template, index) => (
+            {items.map((template, index) => (
               <div key={index} className="">
                 <Card
                   shadow="sm"
@@ -231,13 +283,6 @@ const Page = () => {
                       width="100%"
                       alt={template.title}
                       className="w-full object-cover h-[281px]"
-                      // src={
-                      //   template.fileUrl
-                      //     ? template.fileUrl.startsWith("http")
-                      //       ? template.fileUrl
-                      //       : "/errorImage.png"
-                      //     : "/errorImage.png"
-                      // }
                       src="/bieumau.jpg"
                     />
                     <div className="absolute z-10 bottom-0 left-0 right-0 p-4 opacity-0 group-hover:opacity-100 transition-opacity flex flex-col">
@@ -272,8 +317,10 @@ const Page = () => {
                 </Card>
               </div>
             ))}
-            <div className="flex w-full justify-center">
-              {/* <Pagination
+          </div>
+        )}
+        <div className="flex w-full justify-center items-center mt-10">
+          <Pagination
             isCompact
             showControls
             showShadow
@@ -281,10 +328,8 @@ const Page = () => {
             page={page}
             total={pages}
             onChange={(page) => setPage(page)}
-          /> */}
-            </div>
-          </div>
-        )}
+          />
+        </div>
       </div>
     </>
   );
