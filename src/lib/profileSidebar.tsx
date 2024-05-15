bd658dd82e8fba498cb33c474a9094a0ec8051cf
Cassme1202 <NLDTan120201@gmail.com>
2024-05-15 23:43:09 +0700

    update

--- src/lib/profileSidebar.tsx
+++ src/lib/profileSidebar.tsx
@@ -1,4 +1,4 @@
-import { ProfileSidebarItem, SideNavItem } from "@/constants/types/homeType";
+import { ProfileSidebarItem, SideNavItem } from '@/constants/types/homeType';
 import {
   faHouse,
   faCircleInfo,
@@ -14,48 +14,48 @@ import {
   faClockRotateLeft,
   faBasketShopping,
   faFolderOpen,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const profileSidebar: ProfileSidebarItem[] = [
   {
-    title: "Cài đặt thông tin cá nhân",
-    path: "/profile",
+    title: 'Cài đặt thông tin cá nhân',
+    path: '/profile',
     icon: (
       <FontAwesomeIcon
         icon={faAddressCard}
-        className="w-7 h-7 text-[#FF0004]"
+        className="h-7 w-7 text-[#FF0004]"
       />
     ),
   },
   {
-    title: "Đổi mật khẩu",
-    path: "/profile/changePassword",
-    icon: <FontAwesomeIcon icon={faLock} className="w-7 h-7 text-[#FF0004]" />,
+    title: 'Đổi mật khẩu',
+    path: '/profile/changePassword',
+    icon: <FontAwesomeIcon icon={faLock} className="h-7 w-7 text-[#FF0004]" />,
   },
   {
-    title: "Ví của bạn",
-    path: "/profile/wallet",
+    title: 'Ví của bạn',
+    path: '/profile/wallet',
     icon: (
-      <FontAwesomeIcon icon={faWallet} className="w-7 h-7 text-[#FF0004]" />
+      <FontAwesomeIcon icon={faWallet} className="h-7 w-7 text-[#FF0004]" />
     ),
   },
 
   {
-    title: "Quản lí biểu mẫu",
-    path: "/profile/manageTemplate",
+    title: 'Quản lí biểu mẫu',
+    path: '/profile/manageTemplate',
     icon: (
       <FontAwesomeIcon
         icon={faFileSignature}
-        className="w-7 h-7 text-[#FF0004]"
+        className="h-7 w-7 text-[#FF0004]"
       />
     ),
   },
   {
-    title: "Gói dịch vụ",
-    path: "/profile/servicePack",
+    title: 'Gói dịch vụ',
+    path: '/profile/servicePack',
     icon: (
-      <FontAwesomeIcon icon={faHeadset} className="w-7 h-7 text-[#FF0004]" />
+      <FontAwesomeIcon icon={faHeadset} className="h-7 w-7 text-[#FF0004]" />
     ),
   },
   // {
@@ -66,23 +66,23 @@ export const profileSidebar: ProfileSidebarItem[] = [
   //   ),
   // },
   {
-    title: "Lịch sử giao dịch",
-    path: "/profile/transactionHistory",
+    title: 'Lịch sử giao dịch',
+    path: '/profile/transactionHistory',
     icon: (
       <FontAwesomeIcon
         icon={faClockRotateLeft}
-        className="w-7 h-7 text-[#FF0004]"
+        className="h-7 w-7 text-[#FF0004]"
       />
     ),
   },
-  {
-    title: "Lịch sử mua hàng",
-    path: "/profile/orderHistory",
-    icon: (
-      <FontAwesomeIcon
-        icon={faBasketShopping}
-        className="w-7 h-7 text-[#FF0004]"
-      />
-    ),
-  },
+  // {
+  //   title: "Lịch sử mua hàng",
+  //   path: "/profile/orderHistory",
+  //   icon: (
+  //     <FontAwesomeIcon
+  //       icon={faBasketShopping}
+  //       className="w-7 h-7 text-[#FF0004]"
+  //     />
+  //   ),
+  // },
 ];
