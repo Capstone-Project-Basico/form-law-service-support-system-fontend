bba6ee9f2185b92c3eaa9660ee1db6644462600d
Cassme1202 <NLDTan120201@gmail.com>
2024-05-21 00:49:25 +0700

    update partner contact post recruitment

--- src/components/manageStaff/Partner.tsx
+++ src/components/manageStaff/Partner.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import React, { FormEvent, useEffect, useState } from "react";
+import axios from 'axios';
+import React, { FormEvent, useEffect, useState } from 'react';
 import {
   Table,
   TableHeader,
@@ -22,26 +22,38 @@ import {
   NavbarItem,
   MenuItem,
   Pagination,
-} from "@nextui-org/react";
-import { usePathname } from "next/navigation";
-import { v4 as uuidv4 } from "uuid";
-import { PartnerType } from "@/constants/types/homeType";
-import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
-import { storage } from "@/app/firebase";
-import Image from "next/image";
-import Link from "next/link";
-import { ToastContainer, toast } from "react-toastify";
+  Spinner,
+} from '@nextui-org/react';
+import { usePathname } from 'next/navigation';
+import { v4 as uuidv4 } from 'uuid';
+import { PartnerType } from '@/constants/types/homeType';
+import { ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage';
+import { storage } from '@/app/firebase';
+import Image from 'next/image';
+import Link from 'next/link';
+import { ToastContainer, toast } from 'react-toastify';
+import authHeader from '../authHeader/AuthHeader';
 
 type PartnersProps = {
   partners: PartnerType[];
+  tabs: number;
+  handleDelete: (id: number) => void;
+  unApprove: (id: number) => void;
+  restoreDelete: (id: number) => void;
+  handleApprove: (id: number) => void;
   handleUpdateSubmit: (data: any) => void;
 };
 
 const Partners: React.FC<PartnersProps> = ({
   partners,
+  unApprove,
+  tabs,
+  handleDelete,
+  restoreDelete,
+  handleApprove,
   handleUpdateSubmit,
 }) => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
   const [selectedPartner, setSelectedPartner] = useState<PartnerType | null>(
     null
   );
@@ -54,7 +66,8 @@ const Partners: React.FC<PartnersProps> = ({
   //upload file
   // const [imageUrls, setImageUrls] = useState<string[]>([]);
   // const [imageUrl, setImageUrl] = useState<string>();
-  const imagesListRef = ref(storage, "partners/");
+  const imagesListRef = ref(storage, 'partners/');
+  const [uploading, setUploading] = useState(false);
 
   //search
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -66,7 +79,11 @@ const Partners: React.FC<PartnersProps> = ({
   );
 
   //pagination
-  const [page, setPage] = React.useState(1);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    setPage(1);
+  }, [tabs]);
+
   const rowsPerPage = 5;
 
   const pages = Math.ceil(filteredPartners.length / rowsPerPage);
@@ -80,6 +97,7 @@ const Partners: React.FC<PartnersProps> = ({
 
   //upload update file
   const uploadUpdateFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUploading(true);
     // First, check if the files array is not null and has at least one file
     if (e.target.files && e.target.files.length > 0) {
       const file = e.target.files[0]; // Safely access the first file
@@ -92,7 +110,7 @@ const Partners: React.FC<PartnersProps> = ({
 
       // Listen for state changes, errors, and completion of the upload.
       uploadTask.on(
-        "state_changed",
+        'state_changed',
         (snapshot) => {
           // Optional: monitor upload progress
           const progress =
@@ -101,15 +119,16 @@ const Partners: React.FC<PartnersProps> = ({
         },
         (error) => {
           // Handle unsuccessful uploads
-          console.error("Upload failed", error);
+          console.error('Upload failed', error);
+          setUploading(false); // Hide the spinner on error
         },
         () => {
           // Handle successful uploads on complete
           getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            console.log("File available at", downloadURL);
+            console.log('File available at', downloadURL);
 
             if (!selectedPartner) {
-              console.error("No partner selected for update.");
+              console.error('No partner selected for update.');
               return;
             }
 
@@ -118,14 +137,14 @@ const Partners: React.FC<PartnersProps> = ({
               ...selectedPartner,
               avatar: downloadURL,
             });
-
+            setUploading(false);
             // Optionally: Update the partner's information in the database or state here
             // This might involve calling an API endpoint or updating local state
           });
         }
       );
     } else {
-      console.error("No file selected for upload.");
+      console.error('No file selected for upload.');
     }
   };
 
@@ -136,11 +155,11 @@ const Partners: React.FC<PartnersProps> = ({
         <div className="my-10 flex flex-row">
           <Input
             classNames={{
-              base: "w-full sm:max-w-[10rem] h-10",
-              mainWrapper: "h-full w-96",
-              input: "text-small",
+              base: 'w-full sm:max-w-[10rem] h-10',
+              mainWrapper: 'h-full w-96',
+              input: 'text-small',
               inputWrapper:
-                "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20 ",
+                'h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20 ',
             }}
             placeholder="Từ khóa tìm kiếm .."
             size="sm"
@@ -158,10 +177,10 @@ const Partners: React.FC<PartnersProps> = ({
             <Pagination
               showControls
               classNames={{
-                wrapper: "gap-0 overflow-visible h-8 ",
-                item: "w-8 h-8 text-small rounded-none bg-transparent",
+                wrapper: 'gap-0 overflow-visible h-8 ',
+                item: 'w-8 h-8 text-small rounded-none bg-transparent',
                 cursor:
-                  "bg-gradient-to-b shadow-lg from-default-500 to-default-800 dark:from-default-300 dark:to-default-100 text-white font-bold",
+                  'bg-gradient-to-b shadow-lg from-default-500 to-default-800 dark:from-default-300 dark:to-default-100 text-white font-bold',
               }}
               page={page}
               total={pages}
@@ -174,30 +193,32 @@ const Partners: React.FC<PartnersProps> = ({
           <TableColumn className=" bg-[#FF0004] text-white">
             Tên đối tác
           </TableColumn>
-          <TableColumn className=" justify-center items-center bg-[#FF0004] text-white">
+          <TableColumn className=" items-center justify-center bg-[#FF0004] text-white">
             Hình ảnh
           </TableColumn>
           <TableColumn className=" bg-[#FF0004] text-white">
             Liên kết trang web
           </TableColumn>
-
-          <TableColumn className="flex justify-center items-center bg-[#FF0004] text-white">
+          <TableColumn className=" bg-[#FF0004] text-white">
+            Trạng thái
+          </TableColumn>
+          <TableColumn className="flex items-center justify-center bg-[#FF0004] text-white">
             Tương tác
           </TableColumn>
         </TableHeader>
         <TableBody>
           {items.map((partner, index) => (
             <TableRow key={index}>
-              <TableCell>{partner.name}</TableCell>
+              <TableCell className="font-bold">{partner.name}</TableCell>
               <TableCell>
                 {
                   <Image
                     src={
                       partner.avatar
-                        ? partner.avatar.startsWith("http")
+                        ? partner.avatar.startsWith('http')
                           ? partner.avatar
                           : `/${partner.avatar}`
-                        : "/errorImage.png"
+                        : '/errorImage.png'
                     }
                     alt=""
                     width={100}
@@ -208,24 +229,58 @@ const Partners: React.FC<PartnersProps> = ({
               <TableCell>
                 <Link href={partner.link}>{partner.link}</Link>
               </TableCell>
-
-              <TableCell className="flex gap-2 items-center justify-center">
-                {partner.processStatus === "CHỜ DUYỆT" ? (
-                  <></>
-                ) : (
-                  <>
-                    <Button
-                      className="bg-blue-600 text-white"
-                      onPress={() => {
-                        setSelectedPartner(partner);
-                        onOpenUpdate();
-                      }}
-                    >
-                      Cập nhật
-                    </Button>
-                  </>
-                )}
+              <TableCell>
+                <span style={{ color: partner.delete ? 'red' : 'green' }}>
+                  {partner.delete ? 'Không sử dụng' : 'Đang hoạt động'}
+                </span>
               </TableCell>
+              {!partner.delete ? (
+                <TableCell className="mt-4 flex items-center justify-center gap-2">
+                  {partner.processStatus === 'CHỜ DUYỆT' ? (
+                    <>
+                      {/* <Button
+                        className="bg-blue-600 text-white"
+                        onClick={() => handleApprove(partner.partnerId)}
+                      >
+                        Chập nhận
+                      </Button> */}
+                      <Button
+                        className="bg-blue-600 text-white"
+                        onPress={() => {
+                          setSelectedPartner(partner);
+                          onOpenUpdate();
+                        }}
+                      >
+                        Cập nhật
+                      </Button>
+                      {/* <Button
+                        className="bg-[#FF0004] text-white"
+                        onClick={() => handleDelete(partner.partnerId)}
+                      >
+                        Từ chối và xóa
+                      </Button> */}
+                    </>
+                  ) : (
+                    <>
+                      {/* <Button
+                        className="bg-blue-600 text-white"
+                        onClick={() => unApprove(partner.partnerId)}
+                      >
+                        Chuyển về chờ duyệt
+                      </Button> */}
+                    </>
+                  )}
+                </TableCell>
+              ) : (
+                <TableCell className="flex items-center justify-center gap-2">
+                  <Button
+                    className="bg-blue-600 text-white"
+                    onClick={() => restoreDelete(partner.partnerId)}
+                  >
+                    Khôi phục
+                  </Button>
+                </TableCell>
+              )}
             </TableRow>
           ))}
         </TableBody>
@@ -234,7 +289,7 @@ const Partners: React.FC<PartnersProps> = ({
       {/* update modal */}
       <Modal isOpen={isOpenUpdate} onClose={onCloseUpdate} hideCloseButton>
         <ModalContent>
-          <ModalHeader className="flex flex-col gap-1 text-white text-2xl font-bold bg-[#FF0004] mb-5">
+          <ModalHeader className="mb-5 flex flex-col gap-1 bg-[#FF0004] text-2xl font-bold text-white">
             Cập nhật liên hệ
           </ModalHeader>
           <ModalBody>
@@ -264,6 +319,7 @@ const Partners: React.FC<PartnersProps> = ({
                   type="file"
                   onChange={(e) => uploadUpdateFile(e)}
                 />
+                {uploading && <Spinner />}
                 <Input
                   type="text"
                   label="Link"
@@ -282,7 +338,12 @@ const Partners: React.FC<PartnersProps> = ({
             <Button color="danger" variant="light" onPress={onCloseUpdate}>
               Đóng
             </Button>
-            <Button color="primary" type="submit" form="partner">
+            <Button
+              color="primary"
+              type="submit"
+              form="partner"
+              disabled={uploading}
+            >
               Cập nhật
             </Button>
           </ModalFooter>
--- src/components/manageStaff/Post.tsx
+++ src/components/manageStaff/Post.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import React, { FormEvent, useEffect, useState } from "react";
+import axios from 'axios';
+import React, { FormEvent, useEffect, useState } from 'react';
 import {
   Table,
   TableHeader,
@@ -24,34 +24,43 @@ import {
   Pagination,
   Select,
   SelectItem,
-} from "@nextui-org/react";
-import { usePathname } from "next/navigation";
-import { v4 as uuidv4 } from "uuid";
-import { Category, PostType } from "@/constants/types/homeType";
-import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
-import { storage } from "@/app/firebase";
-import Image from "next/image";
-import dynamic from "next/dynamic";
-import Link from "next/link";
-import { ToastContainer, toast } from "react-toastify";
-import { Editor } from "primereact/editor";
-import { decodeFromBase64, encodeToBase64 } from "@/utils/base64";
+} from '@nextui-org/react';
+import { usePathname } from 'next/navigation';
+import { v4 as uuidv4 } from 'uuid';
+import { Category, PostType } from '@/constants/types/homeType';
+import { ref, getDownloadURL, uploadBytesResumable } from 'firebase/storage';
+import { storage } from '@/app/firebase';
+import Image from 'next/image';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+import { ToastContainer, toast } from 'react-toastify';
+import { Editor } from 'primereact/editor';
+import { decodeFromBase64, encodeToBase64 } from '@/utils/base64';
 
 type PostsProps = {
   posts: PostType[];
+  tabs: number;
+  unApprove: (id: number) => void;
+  handleDelete: (id: number) => void;
+  restoreDelete: (id: number) => void;
+  handleApprove: (id: number) => void;
   handleUpdateSubmit: (data: any) => void;
   categories: Category[];
 };
 
 const Posts: React.FC<PostsProps> = ({
   posts,
+  tabs,
+  unApprove,
+  handleDelete,
+  restoreDelete,
+  handleApprove,
   handleUpdateSubmit,
   categories,
 }) => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
   const [selectedPost, setSelectedPost] = useState<PostType | null>(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
-
   const {
     isOpen: isOpenUpdate,
     onOpen: onOpenUpdate,
@@ -68,11 +77,14 @@ const Posts: React.FC<PostsProps> = ({
   };
   // Filter post based on search title
   const filteredPosts = posts.filter((post) =>
-    (post.title || "").toLowerCase().includes(searchTerm.toLowerCase())
+    (post.title || '').toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   //pagination
   const [page, setPage] = React.useState(1);
+  useEffect(() => {
+    setPage(1);
+  }, [tabs]);
   const rowsPerPage = 5;
 
   const pages = Math.ceil(filteredPosts.length / rowsPerPage);
@@ -86,16 +98,15 @@ const Posts: React.FC<PostsProps> = ({
 
   return (
     <div>
-      <ToastContainer />
       <div>
         <div className="my-10 flex flex-row">
           <Input
             classNames={{
-              base: "w-full sm:max-w-[10rem] h-10",
-              mainWrapper: "h-full w-96",
-              input: "text-small",
+              base: 'w-full sm:max-w-[10rem] h-10',
+              mainWrapper: 'h-full w-96',
+              input: 'text-small',
               inputWrapper:
-                "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20 ",
+                'h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20 ',
             }}
             placeholder="Từ khóa tìm kiếm .."
             size="sm"
@@ -113,10 +124,10 @@ const Posts: React.FC<PostsProps> = ({
             <Pagination
               showControls
               classNames={{
-                wrapper: "gap-0 overflow-visible h-8 ",
-                item: "w-8 h-8 text-small rounded-none bg-transparent",
+                wrapper: 'gap-0 overflow-visible h-8 ',
+                item: 'w-8 h-8 text-small rounded-none bg-transparent',
                 cursor:
-                  "bg-gradient-to-b shadow-lg from-default-500 to-default-800 dark:from-default-300 dark:to-default-100 text-white font-bold",
+                  'bg-gradient-to-b shadow-lg from-default-500 to-default-800 dark:from-default-300 dark:to-default-100 text-white font-bold',
               }}
               page={page}
               total={pages}
@@ -129,71 +140,100 @@ const Posts: React.FC<PostsProps> = ({
           <TableColumn className=" bg-[#FF0004] text-white">
             Tên bài viết
           </TableColumn>
-          {/* <TableColumn className=" bg-[#FF0004] text-white">
-            Nôi dung
-          </TableColumn> */}
-
           <TableColumn className=" bg-[#FF0004] text-white">
             Người tạo
           </TableColumn>
           <TableColumn className=" bg-[#FF0004] text-white">Loại</TableColumn>
-
-          <TableColumn className="flex justify-center items-center bg-[#FF0004] text-white">
+          <TableColumn className="flex items-center justify-center bg-[#FF0004] text-white">
             Tương tác
           </TableColumn>
         </TableHeader>
         <TableBody>
           {items.map((post, index) => (
             <TableRow key={index}>
-              <TableCell>
+              <TableCell className="font-bold">
                 {post.title ? (
                   post.title
                 ) : (
                   <p className="text-[#FF0004]">Bài viết này chưa có tên</p>
                 )}
               </TableCell>
-              {/* <TableCell>{post.content}</TableCell> */}
-
               <TableCell>{post.userName}</TableCell>
               <TableCell>{post.cateName}</TableCell>
-
-              <TableCell className="flex gap-2 items-center justify-center">
-                {post.processStatus === "CHỜ DUYỆT" ? (
-                  <>
-                    <Button
-                      className="bg-green-600 text-white"
-                      onClick={() => {
-                        setSelectedPost(post);
-                        onOpen();
-                      }}
-                    >
-                      Chi tiết
-                    </Button>
-                  </>
-                ) : (
-                  <>
-                    <Button
-                      className="bg-blue-600 text-white"
-                      onPress={() => {
-                        setSelectedPost(post);
-                        onOpenUpdate();
-                      }}
-                    >
-                      Cập nhật
-                    </Button>
-
-                    <Button
-                      className="bg-green-600 text-white"
-                      onClick={() => {
-                        setSelectedPost(post);
-                        onOpen();
-                      }}
-                    >
-                      Chi tiết
-                    </Button>
-                  </>
-                )}
-              </TableCell>
+              {!post.deleted ? (
+                <TableCell className="flex items-center justify-center gap-2">
+                  {post.processStatus === 'CHỜ DUYỆT' ? (
+                    <>
+                      {/* <Button
+                        className="bg-blue-600 text-white"
+                        onClick={() => handleApprove(post.postId)}
+                      >
+                        Chập nhận
+                      </Button> */}
+                      <Button
+                        className="bg-blue-600 text-white"
+                        onPress={() => {
+                          setSelectedPost(post);
+                          onOpenUpdate();
+                        }}
+                      >
+                        Cập nhật
+                      </Button>
+                      {/* <Button
+                        className="bg-[#FF0004] text-white"
+                        onClick={() => handleDelete(post.postId)}
+                      >
+                        Từ chối và xóa
+                      </Button> */}
+                      <Button
+                        className="bg-green-600 text-white"
+                        onClick={() => {
+                          setSelectedPost(post);
+                          onOpen();
+                        }}
+                      >
+                        Chi tiết
+                      </Button>
+                    </>
+                  ) : (
+                    <>
+                      {/* <Button
+                        className="bg-blue-600 text-white"
+                        onClick={() => unApprove(post.postId)}
+                      >
+                        Chuyển sang chờ duyệt
+                      </Button> */}
+                      <Button
+                        className="bg-green-600 text-white"
+                        onClick={() => {
+                          setSelectedPost(post);
+                          onOpen();
+                        }}
+                      >
+                        Chi tiết
+                      </Button>
+                    </>
+                  )}
+                </TableCell>
+              ) : (
+                <TableCell className="flex items-center justify-center gap-2">
+                  <Button
+                    className="bg-blue-600 text-white"
+                    onClick={() => restoreDelete(post.postId)}
+                  >
+                    Khôi phục
+                  </Button>
+                  <Button
+                    className="bg-green-600 text-white"
+                    onClick={() => {
+                      setSelectedPost(post);
+                      onOpen();
+                    }}
+                  >
+                    Chi tiết
+                  </Button>
+                </TableCell>
+              )}
             </TableRow>
           ))}
         </TableBody>
@@ -202,13 +242,13 @@ const Posts: React.FC<PostsProps> = ({
       {/* detail modal */}
       <Modal isOpen={isOpen} onClose={onClose} hideCloseButton>
         <ModalContent
-          style={{ width: "90%", maxWidth: "8000px", height: "90%" }}
+          style={{ width: '80%', maxWidth: '8000px', height: '90%' }}
         >
-          <ModalHeader className="flex flex-col gap-1 text-white text-2xl font-bold bg-[#FF0004] mb-5">
+          <ModalHeader className="mb-5 flex flex-col gap-1 bg-[#FF0004] text-2xl font-bold text-white">
             Chi tiết
           </ModalHeader>
           <ModalBody
-            style={{ maxHeight: "calc(100% - 100px)", overflowY: "auto" }}
+            style={{ maxHeight: 'calc(100% - 100px)', overflowY: 'auto' }}
           >
             {selectedPost && (
               <div className="flex flex-col gap-10">
@@ -230,9 +270,9 @@ const Posts: React.FC<PostsProps> = ({
                 <div className="flex flex-col ">
                   <p className="w-40 pb-3 font-semibold">Nội dung:</p>
                   <div
-                    className="pl-10 content-div border-2 "
+                    className="content-div border-2 pl-10 "
                     dangerouslySetInnerHTML={{
-                      __html: decodeFromBase64(selectedPost.content) || "",
+                      __html: decodeFromBase64(selectedPost.content) || '',
                     }}
                   ></div>
                 </div>
@@ -252,12 +292,12 @@ const Posts: React.FC<PostsProps> = ({
 
       {/* update modal */}
       <Modal isOpen={isOpenUpdate} onClose={onCloseUpdate} hideCloseButton>
-        <ModalContent className="w-[12000px] h-[900px] max-w-none">
-          <ModalHeader className="flex flex-col gap-1 text-white text-2xl font-bold bg-[#FF0004] mb-5">
+        <ModalContent className="h-[850px] w-[1200px] max-w-none">
+          <ModalHeader className="mb-5 flex flex-col gap-1 bg-[#FF0004] text-2xl font-bold text-white">
             Cập nhật liên hệ
           </ModalHeader>
           <ModalBody
-            style={{ maxHeight: "calc(100% - 100px)", overflowY: "auto" }}
+            style={{ maxHeight: 'calc(100% - 100px)', overflowY: 'auto' }}
           >
             {selectedPost && (
               <form
@@ -300,16 +340,16 @@ const Posts: React.FC<PostsProps> = ({
                   ))}
                 </Select>
 
-                <h2 className="font-bold mt-5">Nội dung cho bài viết</h2>
+                <h2 className="mt-5 font-bold">Nội dung cho bài viết</h2>
                 <Editor
                   value={decodeFromBase64(selectedPost.content)}
                   onTextChange={(e) =>
                     setSelectedPost({
                       ...selectedPost,
-                      content: encodeToBase64(e.htmlValue || ""),
+                      content: encodeToBase64(e.htmlValue || ''),
                     })
                   } // Ensures that 'text' is never null
-                  style={{ height: "320px" }}
+                  style={{ height: '400px' }}
                 />
               </form>
             )}
